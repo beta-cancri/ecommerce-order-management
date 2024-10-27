0aaac15ec5dd969d004e0123d1f2c90143b34c36
Diego Chinchay <149448905+beta-cancri@users.noreply.github.com>
2024-10-27 17:15:40 -0500

    changes

--- order-management/src/main/java/Dch/Group/controller/ProductController.java
+++ order-management/src/main/java/Dch/Group/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class ProductController {
     private final ProductService productService;
 
